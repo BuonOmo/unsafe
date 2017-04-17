0208b4de75cddfb583f89b623522e8aec70d22c8
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2017-04-17 14:03:44 -0400

    write to a list file

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var request = require('request');
 var unzip = require('unzip');
 var csv2 = require('csv2');
+var fs = require('fs');
 
+var writer = fs.createWriteStream('./list');
 /*
  * Retrieve data from alexa
  */
 request.get('http://s3.amazonaws.com/alexa-static/top-1m.csv.zip')
   .pipe(unzip.Parse())
   .on('entry', function (entry) {
-    entry.pipe(csv2()).on('data', list => testSafeChuncks(list[0], list[1]));
+    entry.pipe(csv2()).on('data', list => writer.write(list[1] + "\n"));
   })
 ;
 
