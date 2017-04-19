ecb61eaea2a529e6ed0c4ff529812602ff847e27
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2017-04-19 12:07:44 -0400

    read from a list to get threats

--- index.js
+++ index.js
@@ -1,45 +1,53 @@
 var request = require('request');
-var unzip = require('unzip');
-var csv2 = require('csv2');
-var fs = require('fs');
 
-var writer = fs.createWriteStream('./list');
-/*
- * Retrieve data from alexa
- */
-request.get('http://s3.amazonaws.com/alexa-static/top-1m.csv.zip')
-  .pipe(unzip.Parse())
-  .on('entry', function (entry) {
-    entry.pipe(csv2()).on('data', list => writer.write(list[1] + "\n"));
-  })
-;
+var fs = require('fs');
+var writer = fs.createWriteStream('./threats.csv');
+writer.write('url;threatType;platformType;threatEntryType\n');
+
+var alexaList = fs.readFileSync('./list', 'utf-8').split('\n');
+alexaList.pop(); // remove the last unwanted empty line
+var chunkSize = 500;
+
+
+nextChunk(0);
 
 /*
  * testing with google safe browsing API
  */
-function testSafeChuncks(num, url) {
-  var options = {
-    method: 'POST',
-    url: 'https://safebrowsing.googleapis.com/v4/threatMatches:find',
-    qs: {key: 'AIzaSyD8xw-x2GdSI4EfPwnEnCr0X87AZQA81XI'},
-    headers: {'content-type': 'application/json'},
-    body: {
-      client: {clientId: 'polymtl', clientVersion: '1.0.0'},
-      threatInfo: {
-        threatEntryTypes: ['URL'],
-        threatEntries: [{url: url}]
+function nextChunk(idx) {
+  console.log(idx+ ' urls checked ('+((idx*100)/alexaList.length)+'%)' );
+  if (idx + chunkSize < alexaList.length) {
+    var options = {
+      method: 'POST',
+      url: 'https://safebrowsing.googleapis.com/v4/threatMatches:find',
+      qs: {key: 'AIzaSyD8xw-x2GdSI4EfPwnEnCr0X87AZQA81XI'},
+      headers: {'content-type': 'application/json'},
+      body: {
+        client: {clientId: 'polymtl', clientVersion: '1.0.0'},
+        threatInfo: {
+          threatTypes: ["THREAT_TYPE_UNSPECIFIED", "MALWARE", "SOCIAL_ENGINEERING", "UNWANTED_SOFTWARE",
+            "POTENTIALLY_HARMFUL_APPLICATION"],
+          platformTypes: ["ANY_PLATFORM"],
+          threatEntryTypes: ["THREAT_ENTRY_TYPE_UNSPECIFIED", "URL", "EXECUTABLE", "IP_RANGE"],
+          threatEntries: alexaList.slice(idx, idx + chunkSize).map(url => {
+            return {"url": url}
+          })
+        }
+      },
+      json: true
+    };
+    request(options, function (error, response, body) {
+      if (error) {
+        throw new Error(error);
       }
-    },
-    json: true
-  };
-
-  request(options, function (error, response, body) {
-    var curr = num;
-    if (error) {
-      console.log(curr);
-      throw new Error(error);
-    }
-    if (body) console.log(body);
-  });
+      var sc;
+      if (sc = body["matches"]) {
+        for (match of sc) {
+          writer.write(match['threat']['url']+';'+match['threatType']+';'+match['platformType']+';'+match['threatEntryType']+'\n');
+        }
+      }
+      nextChunk(idx + chunkSize)
+    });
+  }
 }
 
