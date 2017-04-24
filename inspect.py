d4c05371841a6480eb2ad4c44cd4f8f7f205cce4
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2017-04-24 15:27:10 -0400

    update inspect script

--- inspect.py
+++ inspect.py
@@ -1,26 +1,18 @@
 #!/usr/bin/env python2.7
 
-import webbrowser
-import psutils
+from subprocess import Popen
 from time import sleep
 
-# Linux
-# chrome_path = '/usr/bin/chromium-browser %s'
+chrome_path = 'C:\Program Files\Google\Chrome\Application\chrome.exe'
 
-# Windows
-chrome_path = 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe %s'
-PROC_NAME = 'chrome.exe'
-
-delay=5
+delay=15
 
 with open('data/threats-merged.csv', 'r') as file:
     for line in file:
         # open url
         url = line.partition(';')[0]
-        webbrowser.get(chrome_path).open(url)
+        proc = Popen([chrome_path, '--disable-web-security', url]
         # wait for malware download
         sleep(delay)
-        # kill processus
-        for proc in psutils.process_iter():
-            if proc.name == PROC_NAME:
-                proc.kill()
+        # kill browser
+        proc.kill()
