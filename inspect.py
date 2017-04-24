6c1d9673e6540bd27e53d14dfb82e615d81f3cd4
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2017-04-24 17:08:54 -0400

    update inspect for less sites

--- inspect.py
+++ inspect.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2.7
 
+from collections import defaultdict
 from subprocess import Popen
 from time import sleep
 
@@ -7,10 +8,18 @@ chrome_path = 'C:\Program Files\Google\Chrome\Application\chrome.exe'
 
 delay=15
 
+c = defaultdict(int)
+count = 0
 with open('data/threats-merged.csv', 'r') as file:
     for line in file:
         # open url
-        url = line.partition(';')[0]
+        url = line.split(';')[0]
+        threat_type = line.split(';')[1]
+        if c[url] > 0 or threat_type == 'SOCIAL_ENGINEERING':
+            continue
+        c[url] += 1
+        count += 1
+        print "%d: %s".format(count, url)
         proc = Popen([chrome_path, '--disable-web-security', url]
         # wait for malware download
         sleep(delay)
